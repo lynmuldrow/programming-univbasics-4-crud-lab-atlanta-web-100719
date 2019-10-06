cdb97429f54b79510b4ffe62a4e4bb46dbfc2519
Lyn Muldrow <lyn.muldrow@flatironschool.com>
2019-10-06 03:52:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,6 @@ def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number = 2
 end
-retrieve_element_from_index[2]
 
 def retrieve_first_element_from_array(array)
   
